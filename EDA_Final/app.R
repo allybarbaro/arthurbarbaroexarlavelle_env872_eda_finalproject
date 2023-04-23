2e2cf2708190290e1604a69bc20000c7c6f08896
maevearthur1 <maeve.arthur@duke.edu>
2023-04-23 12:59:44 -0400

    separated carteret race percentages and numbers into two data frames

--- EDA_Final/app.R
+++ EDA_Final/app.R
@@ -1,51 +1,86 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+#### Load packages ----
 library(shiny)
+library(shinythemes)
+library(tidyverse);library(lubridate)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+#### Load data ----
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+carteret.race <- read_csv('./Data/Processed/Carteret.race.csv')
+carteret.poverty <- read_csv('./Data/Processed/Carteret.poverty.csv')
+carteret.employment <- read_csv('./Data/Processed/Carteret.Employment.csv')
+carteret.age.sex <- read_csv('./Data/Processed/Carteret.age.sex.csv')
+carteret.hurricanes <- read_csv('./Data/Processed/Carteret_Hurricanes_Processed.csv')
+carteret.hurricanes <- read_csv('./Data/Processed/Carteret_Flooding_Processed.csv')
+carteret_shape_correct <- st_read('./Data/Processed/carteret_tracts.shp')
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+#### Define UI ----
+ui <- fluidPage(theme = shinytheme("yeti"),
+                titlePanel("Hurricane and Flood Risk in Carteret County, NC"),
+                sidebarLayout(
+                  sidebarPanel(
+                    
+                    # Select event to plot
+                    selectInput(inputId = "y", 
+                                label = "Nutrient",
+                                choices = c("tn_ug", "tp_ug", "nh34", "no23", "po4"), 
+                                selected = "tp_ug"),
+                    
+                    # Select depth
+                    checkboxGroupInput(inputId = "fill",
+                                       label = "Depth ID",
+                                       choices = unique(nutrient_data$depth_id),
+                                       selected = c(1, 7)),
+                    
+                    # Select lake
+                    checkboxGroupInput(inputId = "shape",
+                                       label = "Lake",
+                                       choices = c("Peter Lake", "Paul Lake"),
+                                       selected = "Peter Lake"),
+                    
+                    # Select date range to be plotted
+                    sliderInput(inputId = "x",
+                                label = "Date",
+                                min = as.Date("1991-05-01"),
+                                max = as.Date("2016-12-31"),
+                                value = c(as.Date("1995-01-01"), as.Date("1999-12-31")))),
+                  
+                  # Output
+                  mainPanel(
+                    plotOutput("scatterplot", brush = brushOpts(id = "scatterplot_brush")), 
+                    tableOutput("mytable")
+                  )))
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
+#### Define server  ----
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  # Define reactive formatting for filtering within columns
+  filtered_nutrient_data <- reactive({
+    nutrient_data %>%
+      filter(sampledate >= input$x[1] & sampledate <= input$x[2]) %>%
+      filter(depth_id %in% input$fill) %>%
+      filter(lakename %in% input$shape) 
+  })
+  
+  # Create a ggplot object for the type of plot you have defined in the UI  
+  output$scatterplot <- renderPlot({
+    ggplot(filtered_nutrient_data(), 
+           aes_string(x = "sampledate", y = input$y, 
+                      fill = "depth_id", shape = "lakename")) +
+      geom_point(alpha = 0.8, size = 2) +
+      theme_classic(base_size = 14) +
+      scale_shape_manual(values = c(21, 24)) +
+      labs(x = "Date", y = expression(Concentration ~ (mu*g / L)), shape = "Lake", fill = "Depth ID") +
+      scale_fill_distiller(palette = "YlOrBr", guide = "colorbar", direction = 1)
+    #scale_fill_viridis_c(option = "viridis", begin = 0, end = 0.8, direction = -1)
+  })
+  
+  # Create a table that generates data for each point selected on the graph  
+  output$mytable <- renderTable({
+    brush_out <- brushedPoints(filtered_nutrient_data(), input$scatterplot_brush)
+  })
+  
 }
 
-# Run the application 
+
+#### Create the Shiny app object ----
 shinyApp(ui = ui, server = server)
